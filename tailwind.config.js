ed3d1f3c0a76ec8de03f338d28707eaf41831aed
Alberto Rivas <albertomasr@hotmail.com>
2023-09-04 14:40:53 -0700

    Proyecto listo para desplegar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,24 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
+
+const color = require('tailwindcss/colors');
+
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  purge: ['./src/**/*{html,js}'],
+  content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      ...color,
     },
+    // extend: {
+    //   backgroundImage: {
+    //     "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+    //     "gradient-conic":
+    //       "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+    //   },
+    // },
   },
-  plugins: [],
-}
+  plugins: [
+    // ...
+    require('@tailwindcss/forms'),
+  ],
+};
